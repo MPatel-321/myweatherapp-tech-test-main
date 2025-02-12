f8d4af19d3159b3b94ef8a2037384cd8775b42e1
Muhammad Patel <muhammad.patel.2@city.ac.uk>
2025-02-12 17:23:33 +0000

    Implemented 'Rain Check' feature

--- src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
+++ src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
@@ -1,5 +1,7 @@
 package com.weatherapp.myweatherapp.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.weatherapp.myweatherapp.model.CityInfo;
 import com.weatherapp.myweatherapp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,4 +28,39 @@ public class WeatherController {
 
   // TODO: given two city names, check which city its currently raining in
 
+  // Checks the current weather conditions in a particular city
+  private String cityCurrentConditions(String city) {
+	  ResponseEntity<CityInfo> cityInfoResponseEntity = forecastByCity(city);
+	  JsonNode cityInfoJsonNode = new ObjectMapper().valueToTree(cityInfoResponseEntity.getBody());
+	  return cityInfoJsonNode.get("currentConditions").get("conditions").asText();
+  }
+  
+  // Takes two city names and checks if none, one, or both of the two corresponding cities are currently experiencing rain
+  @GetMapping("/rainCheck/{city1}/{city2}")
+  public ResponseEntity<String> rainCheck(@PathVariable("city1") String city1, @PathVariable("city2") String city2) {
+	  String regex = "(?i).*rain.*";
+	  
+	  String city1CurrentConditions = cityCurrentConditions(city1);
+	  String city2CurrentConditions = cityCurrentConditions(city2);
+	  
+	  boolean itIsCurrentlyRainingInCity1 = city1CurrentConditions.matches(regex);
+	  boolean itIsCurrentlyRainingInCity2 = city2CurrentConditions.matches(regex);
+	  
+	  String outputText = "<b>Result:</b><br/>";
+	  if(itIsCurrentlyRainingInCity1 && itIsCurrentlyRainingInCity2) {
+		  outputText += "It is currently raining in both '" + city1 + "' and '" + city2 + "'.";
+	  }
+	  else if(itIsCurrentlyRainingInCity1) {
+		  outputText += "It is currently raining in '" + city1 + "', but not in '" + city2 + "'.";
+	  }
+	  else if(itIsCurrentlyRainingInCity2) {
+		  outputText += "It is currently raining in '" + city2 + "', but not in '" + city1 + "'.";
+	  }
+	  else {
+		  outputText += "It is currently not raining in either '" + city1 + "' or '" + city2 + "'.";
+	  }
+	  
+	  outputText += "<br/><br/><b>Reason:</b><br/>Current conditions in '" + city1 + "': " + city1CurrentConditions + ".<br/>Current conditions in '" + city2 + "': " + city2CurrentConditions + ".";
+	  return ResponseEntity.ok(outputText);
+  }
 }
